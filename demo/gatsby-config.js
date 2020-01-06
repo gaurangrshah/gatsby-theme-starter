80dd9de39fe2a115c11f952353db3c22adc2913c
gaurangrshah <gaurang.r.shah@gmail.com>
2020-01-05 20:50:11 -0500

    initial commit

---
+++ demo/gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: "Gatsby Theme Stater",
+    siteUrl: `https://gatsby-theme-stater.netlify.com`,
+    description: `gatsby theme mdx demo page & documentation. forked from Horacio Herrera (@hhg2288)`,
+  },
+  plugins: [{
+    resolve: "@gaurang.r.shah@gmail.com/gatsby-theme-starter"
+  }],
+}
